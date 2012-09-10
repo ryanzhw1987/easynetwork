e500fd437c57dda0ee06927afb3f6bb5f42de187
xmulyj@gmail.com <xmulyj@gmail.com@068ce12d-73bd-f553-74b1-80c710c7b3ec>
2012-09-10 11:42:05 +0000

    threadpool

--- trunk/libnetframework/AppFramework_Sample/ConnectThread.h
+++ trunk/libnetframework/AppFramework_Sample/ConnectThread.h
@@ -9,16 +9,20 @@
 
 #include "Thread.h"
 #include "ConnectAccepter.h"
+#include "Queue.h"
 
 class ConnectThread:public Thread, public ConnectAccepter
 {
 public:
 	//实现ConnectAccepter:接收一个新的连接请求
-	bool accept(SocketHandle trans_fd);
+	bool accept(SocketHandle trans_fd){return m_connect_queue.push(trans_fd);}
+public:
+	ConnectThread():m_connect_queue(true){}
 private:
 	//线程安全队列
-
+	Queue<SocketHandle> m_connect_queue;
 };
+
 #endif //_LIB_CONNECT_THREAD_H_
 
 
