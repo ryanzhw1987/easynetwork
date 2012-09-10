6125b155478a91ab20832bbb9642217f4c5123e3
xmulyj@gmail.com <xmulyj@gmail.com@068ce12d-73bd-f553-74b1-80c710c7b3ec>
2012-09-10 11:23:38 +0000

    threadpool

--- trunk/libnetframework/ThreadPool.h
+++ trunk/libnetframework/ThreadPool.h
@@ -20,10 +20,13 @@ public:
 protected:
 	//创建一个线程
 	virtual Thread* create_thread()=0;
+	unsigned int get_thread_num(){return m_thread_num;}
+	Thread* get_thread(unsigned int thread_index){return thread_index<m_thread_num?m_thread_array[thread_index]:NULL;}
 private:
 	bool m_inited;
 	Thread **m_thread_array;
 	unsigned int m_thread_num;
+
 };
 
 #endif //_LIB_THREAD_POOL_H_
